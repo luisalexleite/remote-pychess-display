13e1a629490e4600854b98f4651d056a02a30b81
Eduardo Capela <72978307+EduardoCapela@users.noreply.github.com>
2021-03-16 23:17:00 +0000

    teste
    teste

---
+++ android/app/src/main/java/com/example/chess/SplashScreen.java
@@ -0,0 +1,65 @@
+package com.example.chess;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.example.chess.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final String TAG = "SplashScreen";
+    //FireStore instance
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final CollectionReference petsRef = db.collection("Pet");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final String[] darkModeValues = getResources().getStringArray(R.array.dark_mode_values);
+        // The apps theme is decided depending upon the saved preferences on app startup
+        String pref = PreferenceManager.getDefaultSharedPreferences(this)
+                .getString(getString(R.string.dark_mode), getString(R.string.dark_mode_def_value));
+        // Comparing to see which preference is selected and applying those theme settings
+        if (pref.equals(darkModeValues[0]))
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        if (pref.equals(darkModeValues[1]))
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        if (pref.equals(darkModeValues[2]))
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        if (pref.equals(darkModeValues[3]))
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        //get user instance
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        new Handler().postDelayed(() -> {
+            Intent i;
+            if (user != null) {
+                // User is signed in
+                i = new Intent(SplashScreen.this, MainActivity.class);
+            } else {
+                // User is signed out
+                i = new Intent(SplashScreen.this, LoginActivity.class);
+            }
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(i);
+            finish();
+        }, 2000);
+    }
+
+}
