44ff470c6b5acac27b018f9dd5bde94fb3c20a21
luisalexleite <luis.alexleite@gmail.com>
2021-03-23 00:20:58 +0000

    gitignore

--- raspberry/remote-pychess.py
+++ raspberry/remote-pychess.py
@@ -5,7 +5,72 @@ import time
 import tkinter
 import chess
 import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import db
 from lib.chessengine import checkMove
+
+#requisitos do firebase
+cred = credentials.Certificate('raspberry/cred/remote-pychess-f8ba9c6e343c.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://remote-pychess-default-rtdb.europe-west1.firebasedatabase.app/'
+})
+
+def end_game():
+    #fechar jogo
+    pyautogui.hotkey('ctrl', 'q')
+
+ref = db.reference('/teste')
+print(ref.get())
+
+def makeMove(game):
+    """
+    logica da função feita falta ligar à bd - firestore e realtime database
+    """
+    #enquanto o jogo decorrer
+    while state == 1:
+        #se hover algum moviento disponível
+        if movimento_disponivel:
+            #verificar se movimento é válido
+            valid, checkmate, stalemate, nomaterial, claim, repetition, board = checkMove(board, 'move')
+
+            #verificar se há vitória, empate ou se continua o jogo
+            if (valid == True):
+                if (checkmate == True):
+                    print("checkmate")  
+                    pyautogui.write()
+                    pyautogui.press('enter')
+                    print()
+                elif (stalemate == True):
+                    print("stalemate")
+                    pyautogui.write()
+                    pyautogui.press('enter')
+                    print()
+                elif (nomaterial == True):
+                    print("Insufficent Material")
+                    pyautogui.write()
+                    pyautogui.press('enter')
+                    print()
+                elif (repetition == True):
+                    print("Repetition")
+                    pyautogui.write()
+                    pyautogui.press('enter')
+                    print()
+                elif (claim == True):
+                    print("Claim Draw")
+                    pyautogui.write()
+                    pyautogui.press('enter')
+                    print()
+                else:
+                    pyautogui.write()
+                    pyautogui.press('enter')
+                    print()
+                    print('Movimento Válido')
+            else:
+                print('Movimento inválido')
+        else:
+            #terminar o jogo
+            end_game()
 
 def start_game(white, black, mode):
 
@@ -65,43 +130,5 @@ def start_game(white, black, mode):
         #iniciar jogo em normal
         pyautogui.click(int(width)/2.167042889,int(height)/1.839863714)
         pyautogui.press('enter')
-
-def makeMove(board,move,game):
-    valid, checkmate, stalemate, nomaterial, claim, repetition, board = checkMove(board, move)
-    if (valid == True):
-        if (checkmate == True):
-            print("checkmate")  
-            pyautogui.write(move)
-            pyautogui.press('enter')
-            print(move)
-        elif (stalemate == True):
-            print("stalemate")
-            pyautogui.write(move)
-            pyautogui.press('enter')
-            print(move)
-        elif (nomaterial == True):
-            print("Insufficent Material")
-            pyautogui.write(move)
-            pyautogui.press('enter')
-            print(move)
-        elif (repetition == True):
-            print("Repetition")
-            pyautogui.write(move)
-            pyautogui.press('enter')
-            print(move)
-        elif (claim == True):
-            print("Claim Draw")
-            pyautogui.write(move)
-            pyautogui.press('enter')
-            print(move)
-        else:
-            pyautogui.write(move)
-            pyautogui.press('enter')
-            print(move)
-            print('Movimento Válido')
-    else:
-        print('Movimento inválido')
-        
-def end_game():
-    #fechar jogo
-    pyautogui.hotkey('ctrl', 'q')
+    
+    makeMove('something')
