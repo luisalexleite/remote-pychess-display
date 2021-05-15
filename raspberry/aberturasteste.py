2e5334dab500dcaa0fcb15762ebf4b98ff297b28
luisalexleite <luis.alexleite@gmail.com>
2021-05-15 02:21:29 +0100

    falta acrescentar aberturas ao prorama principal

--- raspberry/aberturasteste.py
+++ raspberry/aberturasteste.py
@@ -4,34 +4,42 @@ from lib.chessengine import checkMove
 
 
 board = chess.Board().fen()
-game = chess.pgn.Game()
 movearr = []
 boardreset = chess.Board()
 exporter = chess.pgn.StringExporter(
     headers=False, variations=False, comments=False)
-
 eco = open("eco.pgn")
-first_game = chess.pgn.read_game(eco)
-sec_game = chess.pgn.read_game(eco)
-print(first_game.headers)
-
-
+ECO = ""
+opening = ""
+variation = ""
+err = 0
 while True:
     inp = input()
     teste = checkMove(board, inp)
     state = teste[0]
-    if (state == True):
-        movearr.append(inp)
-
-    print(movearr)
-    boardreset = chess.Board()
-    movecheck = chess.Board().variation_san(
-        [boardreset.push_san(m) for m in movearr])
-
-    print(movecheck)
-
-    if (movecheck + " *" == sec_game.accept(exporter)):
-        print(sec_game.headers['Opening'])
     board = teste[6]
 
-    print(teste[6])
+    if (state == True):
+        movearr.append(inp)
+        boardreset = chess.Board()
+        movecheck = chess.Board().variation_san(
+            [boardreset.push_san(m) for m in movearr]) + " *"
+
+        print(movecheck)
+        while True:
+            game = chess.pgn.read_game(eco)
+            if game is None:
+                break
+            else:
+                san = game.accept(exporter)
+                if (movecheck in san):
+                    ECO = game.headers['ECO']
+                    opening = game.headers['Opening']
+                    try:
+                        variation = game.headers['Variation']
+                    except:
+                        variation = ""
+
+                    break
+
+        print(ECO, opening, variation)
