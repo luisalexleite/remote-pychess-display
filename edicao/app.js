df75bebcc50feb9c6adc7ff8b10c7a68996d4e19
GrandePedro6230 <amarelo6230@gmail.com>
2021-06-01 19:22:07 +0100

    Pág de Jogadores (falta retirar o botão carrega-me)

--- edicao/app.js
+++ edicao/app.js
@@ -13,18 +13,28 @@
     appId: "1:914254287981:web:091c8214af93fd480a5b64",
     measurementId: "G-YX8YYWSDH0"
   };
-  
+
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  var app = firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
   var database = firebase.database();
-  var firestore = firebase.firestore();
+  var firestore = firebase.firestore(app);
 
   var ref = database.ref('games');
+
+  document.getElementById("gravar").addEventListener("click", function () {
+    firebase.database().ref('games/' + "bitchboy").set({
+      blacks: "2ZZ9Ozfg4UXEd1dl2wzDI5SAnsg2",
+      method: 0,
+      result: 0,
+      state: 0,
+      type: 0,
+      whites: "K3aelVv7aVSrAChLLfkqxd7pG412",
+    })
+  })
   
   ref.on('child_added', (data) => {
-       var key = data.key;
        var val = data.val();
        var state = val['state'];
        if (state == 0) {
@@ -33,25 +43,59 @@
           $(".wrapper").slideUp(3000);
           setTimeout(menujogadores, 3200);
           }
-          function menujogadores () {
-            $(".jogadores").show().animate({opacity: '1'},2500);
-          }
+        function menujogadores () {
+          $(".jogadores").show().animate({opacity: '1'},2500);
+        }
         $(".loader").fadeOut(2000);
         alterarmenu();
-      
-        setTimeout(queryjogadores, 3000);
 
-        var whites = val["whites"];
-        var blacks = val["blacks"];
+        fswhites = firestore.collection('profile').doc(val['whites']);
+        fsblacks = firestore.collection('profile').doc(val['blacks']);
 
-        document.getElementById("brancas").innerText = whites.value;
+        fswhites.get().then((doc) => {
+          if (doc.exists) {
+            var array = doc.data()
+            document.getElementById("brancas").innerText = array['username'];
+            document.getElementById("imgbrancas").src = array['image'];
+            document.getElementById("ratingbrancas").innerText = "Elo: " + array['rating'];
+          } else {
+            console.log("Im gay!");
+          }
+        }).catch((error) => {
+          console.log("Erro:", error);
+        })
+
+        fsblacks.get().then((doc) => {
+          if (doc.exists) {
+            var array = doc.data()
+            document.getElementById("pretas").innerText = array['username'];
+            document.getElementById("imgpretas").src = array['image'];
+            document.getElementById("ratingpretas").innerText = "Elo: " + array['rating'];
+          } else {
+            console.log("Im gay!");
+          }
+        }).catch((error) => {
+          console.log("Erro:", error);
+        })
+
+
+        if (val['type'] == 0) {
+          document.getElementById("tipojogo").innerText = "Blitz";
+          document.getElementById("timer").innerText = "3 : 00";
+        } else if (val['type'] == 1) {
+          document.getElementById("tipojogo").innerText = "Rápido";
+          document.getElementById("timer").innerText = "10 : 00";
+        } else if (val['type'] == 2) {
+          document.getElementById("tipojogo").innerText = "Normal";
+          document.getElementById("timer").innerText = "20 : 00";
+        }
         
         //firebase.database().ref('inline/' + Date.now()) .set({
           //game : key
         //});
-          window.location = 'http://localhost:8000/startgame.php?id=' + key;
+         // window.location = 'http://localhost:8000/startgame.php?id=' + key;
        }
   });
 
-  alterarEcra();
+  //alterarEcra();
 });
