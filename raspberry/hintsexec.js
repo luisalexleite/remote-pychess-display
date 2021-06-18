cf406613ddc93e585406216a42d06d4b07ed1cf7
luisalexleite <luis.alexleite@gmail.com>
2021-06-18 22:24:46 +0100

    evitar repetições

--- raspberry/hintsexec.js
+++ raspberry/hintsexec.js
@@ -2,17 +2,27 @@ var hint = Math.floor(Math.random() * parseInt(Object.keys(hints).length)) + 1;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
 
 var hint = Math.floor(Math.random() * parseInt(Object.keys(hints).length)) + 1;
 
-async function alterarEcra () {
+async function alterarEcra() {
     while (true) {
         document.getElementById("title").innerText = hints[hint]['title'];
         document.getElementById("peqtext").innerText = hints[hint]['text'];
         document.getElementById("board").src = hints[hint]['img'];
         document.getElementById("movimentos").innerText = hints[hint]['moves'];
-        hint = Math.floor(Math.random() * parseInt(Object.keys(hints).length)) + 1;
+
         await sleep(parseInt(hints[hint]['time'] + "000"))
+
+        do {
+            random = Math.floor(Math.random() * parseInt(Object.keys(hints).length)) + 1;
         }
+        while (random == hint)
+
+        hint = random;
+        console.log(hint);
+
+
+    }
 }
